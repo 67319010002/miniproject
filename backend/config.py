001f7366e1f454a5ca68dfff2295c8050f55fdd5
67319010002 <67319010002@loeitech.ac.th>
2025-08-14 15:19:26 +0700

    edit mongo

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,6 @@ class Config:
     SECRET_KEY = 'your-secret-key'
     JWT_SECRET_KEY = 'your-jwt-secret'
     MONGODB_SETTINGS = {
-        'db': 'mario_notes',
-        'host': 'localhost',
-        'port': 27017
+        'db': 'sharenotes',
+        'host': 'mongodb://loeitech_admin_2:G7%23u4sK!8zWb@202.29.231.188:27019/?authSource=admin'
     }
